0dedcbd886102fbec6a2d765ac2b9a8e984f95d3
Yousra <ybibi@ggc.edu>
2023-02-28 00:10:31 -0800

    added comment line.

--- src/sumOfDigits.java
+++ src/sumOfDigits.java
@@ -8,7 +8,7 @@ public class sumOfDigits {
         int digit = num;
         int sum = 0;
 
-        while(num! = 0)
+        while(num != 0)
         {
             sum = sum + num % 10;
             num = num / 10;
